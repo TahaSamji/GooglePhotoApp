dbdea88968e7c752191995032eee963c2a40456a
Right 97 <Right.97@rs.net>
2024-08-15 17:39:20 +0500

    Vercel commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
+
+
 
 function App() {
+  const [image,setImage] = useState("");
+  const [file, setfile] = useState();
+  const handleChange = function (e) {
+   console.log(e.target.files[0]);
+    setfile(e.target.files[0]);
+
+  }
+  const Authenticate = async () => {
+    try {
+      const res = await axios({
+        url: "http://localhost:8000/Authenticate",
+        method: 'get',
+      });
+      if (res.data) {
+        
+        window.location.href = res.data;
+       
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const Getdata = async () => {
+    try {
+      const res = await axios({
+        url: "http://localhost:8000/GetData",
+        method: 'get',
+
+      });
+      if (res.data) {
+        
+        console.log(res.data);
+       
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const UploadImage = async () => {
+    try {
+      const formData = new FormData();
+      formData.append("UploadImage",file);
+      const res = await axios({
+        url: "http://localhost:8000/UploadImage",
+        method: 'post',
+        data:formData
+      });
+      if (res.data) {
+        
+        console.log(res.data);
+       
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+
+
   return (
+
+    //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
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <button onClick={Authenticate}>Authenticate</button>
+     <button onClick={Getdata}>GetImages</button>
+     <input onChange={handleChange} type='file'></input>
+     <button onClick={UploadImage}>Upload Image</button>
+     
     </div>
   );
 }
